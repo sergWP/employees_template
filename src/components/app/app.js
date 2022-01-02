590d916dd8006e30fef4d609ccf1a471e216714a
sergWP <kuzmenko.sergei@gmail.com>
2022-01-02 22:45:30 +0200

    main component App init

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,19 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 function App() {
     return(
-        <div className={'text-center my-auto'}>
-            Hello
+        <div className='max-w-5xl mx-auto app'>
+            <AppInfo/>
+            <div className='bg-slate-200 p-4 rounded-md'>
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            <EmployeesList/>
+            <EmployeesAddForm/>
         </div>
     )
 }
