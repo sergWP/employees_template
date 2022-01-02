a585a05d82c1b1874a1e0fe95cf2444eccc4ac83
sergWP <kuzmenko.sergei@gmail.com>
2022-01-02 22:44:07 +0200

    component info init

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,16 @@
+const AppInfo = () => {
+    return(
+        <div className='
+                p-4
+                my-4
+                bg-slate-400
+                text-white
+                rounded-md'>
+            <h1 className='text-4xl'>Учет сотрудников</h1>
+            <h2 className='text-2xl'>Общее число сотрудников: </h2>
+            <h2 className='text-2xl'>Премию получат: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
