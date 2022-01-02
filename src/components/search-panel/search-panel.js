1d81145bb48e9a9ad3282003f77f0566dabb378a
sergWP <kuzmenko.sergei@gmail.com>
2022-01-02 22:44:58 +0200

    component search init

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,10 @@
+const SearchPanel = () => {
+    return(
+        <input 
+            type={'text'}
+            placeholder="Найти сотрудника"
+            className="w-full py-2 px-4 rounded-md"/>
+    )
+}
+
+export default SearchPanel;
