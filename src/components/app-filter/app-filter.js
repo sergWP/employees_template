8e76d6d49292dbb84f0121b6fedf3d803f7a2fdb
sergWP <kuzmenko.sergei@gmail.com>
2022-01-02 22:43:51 +0200

    component filter init

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,23 @@
+const AppFilter = () => {
+    return(
+        <div className="text-white flex mt-4">
+            <button 
+                type="button" 
+                className="outline outline-slate-400 outline-1 px-3 py-2 bg-slate-400 rounded-l-md hover:bg-slate-500 hover:text-white">
+                    Все сотрудники
+            </button>
+            <button 
+                type="button" 
+                className="outline outline-slate-400 outline-1 px-3 py-2 text-slate-600 hover:bg-slate-500 hover:text-white">
+                    На повышение
+            </button>
+            <button 
+                type="button" 
+                className="outline outline-slate-400 outline-1 px-3 py-2 text-slate-600 rounded-r-md hover:bg-slate-500 hover:text-white">
+                    З/П больше $1000
+            </button>
+        </div>
+    )
+}
+
+export default AppFilter;
