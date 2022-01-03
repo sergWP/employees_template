f162a4e6bcaf76565c3285dddb094051d9aed454
sergWP <kuzmenko.sergei@gmail.com>
2022-01-03 09:42:25 +0200

    92 lesson (props examples)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,6 +4,33 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+function WhoAmI({name, surname, url, urlText}) {
+    return(
+        <div>
+            <h1>My name {name}, surname - {surname}</h1>
+            <a href={url}>{urlText}</a>
+        </div>
+    )
+}
+
+function AmI({name, surname, url, urlText}) {
+    return(
+        <div>
+            <h1>My name {name.firstName}, surname - {surname}</h1>
+            <a href={url}>{urlText}</a>
+        </div>
+    )
+}
+
+function User({name, surname, url, urlText}) {
+    return(
+        <div>
+            <h1>My name {name()}, surname - {surname}</h1>
+            <a href={url}>{urlText}</a>
+        </div>
+    )
+}
+
 function App() {
     return(
         <div className='max-w-5xl mx-auto app'>
@@ -14,6 +41,9 @@ function App() {
             </div>
             <EmployeesList/>
             <EmployeesAddForm/>
+            <WhoAmI name='John' surname='Smith' url='react.io' urlText='REACT'/>
+            <AmI name={{firstName: 'Alex'}} surname='Claus' url='react.io' urlText='REACT'/>
+            <User name={() => { return 'Peter' }} surname='Forter' url='react.io' urlText='REACT'/>
         </div>
     )
 }
