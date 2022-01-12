3c66e6ce2b504187acb61f0158bfd7d13e744ca4
sergWP <kuzmenko.sergei@gmail.com>
2022-01-12 09:02:39 +0200

    search component to Class

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,10 +1,14 @@
-const SearchPanel = () => {
-    return(
-        <input 
-            type={'text'}
-            placeholder="Найти сотрудника"
-            className="w-full py-2 px-4 rounded-md"/>
-    )
+import { Component } from 'react';
+
+class SearchPanel extends Component {
+    render() {
+        return(
+            <input 
+                type={'text'}
+                placeholder="Найти сотрудника"
+                className="w-full py-2 px-4 rounded-md"/>
+        )
+    }
 }
 
 export default SearchPanel;
