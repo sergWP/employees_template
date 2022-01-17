4b87a337ca024ac85391c8cc6c9e95a46d253a56
sergWP <kuzmenko.sergei@gmail.com>
2022-01-17 10:13:03 +0200

    dynamic styles in component

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,20 +1,22 @@
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: 'all', label: 'Все сотрудники'},
-        {name: 'star', label: 'На повышение'},
-        {name: 'top', label: 'З/П больше $1000'}
+        {name: 'all', label: 'Все сотрудники', colored: false},
+        {name: 'star', label: 'На повышение', colored: false},
+        {name: 'top', label: 'З/П больше $1000', colored: true}
     ];
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = props.filter === name;
         const clazz = active ? 'bg-slate-400' : 'text-slate-600';
+        const style = colored ? {color: 'red'} : null;
         return (
             <button 
                 type="button" 
                 onClick={() => props.onFilter(name)}
                 filter={name}
                 key={name}
-                className={`outline outline-slate-400 outline-1 px-3 py-2 hover:bg-slate-500 hover:text-white ${clazz}`}>
+                className={`outline outline-slate-400 outline-1 px-3 py-2 hover:bg-slate-500 hover:text-white ${clazz}`}
+                style={style}>
                     {label}
             </button>
         )
